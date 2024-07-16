31f1e5a8b4d53b558dc12735db142707774cb76a
YutoShimizu <yuto6409@gmail.com>
2024-07-16 21:28:00 +0900

    リレーション機能　追加

--- board_app/app/models.py
+++ board_app/app/models.py
@@ -1,10 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 class Board(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='boards', null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     
+    def __str__(self):
+        return self.title
--- board_app/app/views.py
+++ board_app/app/views.py
@@ -4,8 +4,17 @@ from .models import Board
 from django.contrib.auth.views import LoginView
 from django.contrib.auth import logout
 from django.contrib.auth.decorators import login_required
+from functools import wraps
 
-# Create your views here.
+def user_owns_board(view_func):
+    @wraps(view_func)
+    def wrapper(request, pk):
+        board = get_object_or_404(Board, pk=pk)
+        if board.user == request.user:
+            return view_func(request, pk)
+        else:
+            return redirect('index')
+    return wrapper
 
 def index(request):
     boards = Board.objects.all().order_by('-updated_at')
@@ -21,6 +30,7 @@ def create(request):
     if request.method == 'POST':
         form = BoardForm(request.POST)
         if form.is_valid():
+            form.instance.user = request.user
             form.save()
             return redirect('index')
     else:
@@ -33,12 +43,14 @@ def show(request, pk):
     return render(request, 'show.html', {'board': board})
 
 @login_required
+@user_owns_board
 def edit(request, pk):
     board = Board.objects.get(pk=pk)
     form = BoardForm(instance=board)
     return render(request, 'edit.html', {'form': form, 'board': board})
 
 @login_required
+@user_owns_board
 def update(request, pk):
     board = Board.objects.get(pk=pk)
     if request.method == 'POST':
@@ -51,13 +63,19 @@ def update(request, pk):
     return render(request, 'edit.html', {'form': form, 'board': board})
 
 @login_required
+@user_owns_board
 def delete(request, pk):
     board = Board.objects.get(pk=pk)
     if request.method == 'POST':
         board.delete()
         return redirect('index')
-    return redirect('index')
+    return redirect('index',pk=pk)
 
+@login_required
+def my_boards(request):
+    user = request.user
+    boards = user.boards.all()
+    return render(request, 'my_boards.html', {'boards':boards})
 
 class CustomLoginView(LoginView):
     template_name = 'registration/login.html'
