95de7c2600f41061fe4541667e9e81f1d77d7573
YutoShimizu <yuto6409@gmail.com>
2024-07-24 20:23:48 +0900

    提出前修正

--- board_app/app/views.py
+++ board_app/app/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect
-from .forms import BoaredFoorm, SignUpForm
+from .forms import BoardForm, SignUpForm
 from .models import Board
 from django.contrib.auth.views import LoginView
+from django.contrib.auth import logout
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
@@ -9,40 +11,46 @@ def index(request):
     boards = Board.objects.all().order_by('-updated_at')
     return render(request, 'index.html', {'boards': boards})
 
+@login_required
 def new(request):
-    form = BoaredFoorm()
+    form = BoardForm()
     return render(request, 'new.html', {'form': form})
 
+@login_required
 def create(request):
     if request.method == 'POST':
-        form = BoaredFoorm(request.POST)
+        form = BoardForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect('index')
     else:
-        form = BoaredFoorm()
+        form = BoardForm()
     return render(request, 'new.html', {'form': form})
 
+@login_required
 def show(request, pk):
     board = Board.objects.get(pk=pk)
     return render(request, 'show.html', {'board': board})
 
+@login_required
 def edit(request, pk):
     board = Board.objects.get(pk=pk)
-    form = BoaredFoorm(instance=board)
+    form = BoardForm(instance=board)
     return render(request, 'edit.html', {'form': form, 'board': board})
 
+@login_required
 def update(request, pk):
     board = Board.objects.get(pk=pk)
     if request.method == 'POST':
-        form = BoaredFoorm(request.POST, instance=board)
+        form = BoardForm(request.POST, instance=board)
         if form.is_valid():
             form.save()
             return redirect('show', pk=pk)
     else:
-        form = BoaredFoorm(instance=board)
+        form = BoardForm(instance=board)
     return render(request, 'edit.html', {'form': form, 'board': board})
 
+@login_required
 def delete(request, pk):
     board = Board.objects.get(pk=pk)
     if request.method == 'POST':
@@ -55,17 +63,22 @@ class CustomLoginView(LoginView):
     template_name = 'registration/login.html'
 
 def logout_view(request):
-    pass
+    logout(request)
+    return redirect('index')
 
 def signup(request):
     if request.method == 'POST':
         form = SignUpForm(request.POST)
         if form.is_valid():
-            form.save
+            form.save()
             return redirect('login')
+        else:
+            return redirect('index')
     else:
         form = SignUpForm()
     return render(request, 'registration/signup.html', {'form': form})
 
+@login_required
 def profile(request):
-    pass
+    user = request.user
+    return render(request, 'accounts/profile.html', {'user': user})
